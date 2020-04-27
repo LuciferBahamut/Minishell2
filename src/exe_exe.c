90863c3b83f845fc6e122e2c7c857bb22d4c0ca1
Quentin TREHEUX <quentin.treheux@epitech.eu>
2020-04-27 16:53:26 +0200

    [ADD] if exe is dir

--- src/exe_exe.c
+++ src/exe_exe.c
@@ -8,13 +8,31 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include "mysh.h"
+
+static int check_is_dir(char **arg)
+{
+    char **split = my_split(arg[0], '/');
+    struct stat st;
+
+    stat(split[1], &st);
+    if (S_ISDIR(st.st_mode)) {
+        my_putstr_error(arg[0]);
+        my_putstr_error(PERMDEN);
+        return (ERROR);
+    }
+    return (SUCCESS);
+}
 
 void exe_without_path(mysh_t *m)
 {
     pid_t pid = 0;
     char *const *arg = m->arg;
 
+    if (check_is_dir(m->arg) == ERROR)
+        return;
     if (m->pipe == 6) {
         execve(m->arg[0], arg, m->envp);
         return;
@@ -31,6 +49,8 @@ void exe_with_path(mysh_t *m, int j)
     pid_t pid = 0;
     char *const *arg = m->arg;
 
+    if (check_is_dir(m->arg) == ERROR)
+        return;
     if (m->pipe == 6) {
         execve(m->arg[0], arg, m->envp);
         return;
